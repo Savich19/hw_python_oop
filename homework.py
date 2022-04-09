38e56cdc9c04d347a88265f960aec2a3a375e012
Александр Савич <rcdrifts@ya.ru>
2022-04-09 03:31:11 +0300

    Final commit - 2

--- homework.py
+++ homework.py
@@ -55,7 +55,7 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError('<Не могу рассчитать калории ¯\_(ツ)_/¯>')
+        raise NotImplementedError('<Не могу рассчитать калории>')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
